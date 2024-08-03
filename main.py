099368e37e8e3af48cb67db290f3f1bf6871cb3a
janellekwek <158569843+janellekwek@users.noreply.github.com>
2024-08-03 17:51:34 +0000

    new form editing

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def admin():
         if password == "Admin12345":
             token = str(uuid.uuid4())
             tokens[token] = True
-            return redirect(f'/admin?token={token}')
+            return redirect(f'/admin/control?token={token}')
         else:
             return redirect('/admin?error=True')
 
@@ -45,6 +45,30 @@ def admin():
 @app.route("/logout")
 def logout():
     return redirect('/admin?logout=true')
+
+@app.route("/admin/control", methods=["GET", "POST"])
+def admin_dashboard():
+    token = request.args.get('token')
+    if token not in tokens:
+        return redirect('/admin?error=True')
+
+    if request.method == "POST":
+        if 'form1' in request.form:
+            # Handle form1 submission
+            form1_data = request.form
+            print("Form 1 data received:", form1_data)
+            # Process form1_data as needed
+            return redirect('/admin/control?token=' + token)
+
+        elif 'form2' in request.form:
+            # Handle form2 submission
+            form2_data = request.form
+            print("Form 2 data received:", form2_data)
+            # Process form2_data as needed
+            return redirect('/admin/control?token=' + token)
+
+
+    return render_template("control.html")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
