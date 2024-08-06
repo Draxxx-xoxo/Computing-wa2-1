7f94db3c345a4a45531bcb41e8c1967e01859e91
janellekwek <158569843+janellekwek@users.noreply.github.com>
2024-08-06 16:07:58 +0000

    sql db

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from flask import Flask, render_template, url_for, request, redirect
 import uuid
-import csv
+import sqlite3
 app = Flask(__name__)
 
-def readcsv(file_name):
-    content = []
-    with open(file_name, "r") as file:
-         reader = csv.reader(file)
-         for row in reader:
-             content.append(row)
-    return content
-    
-def addrow(file_name, new_row):
-    with open(file_name, "a") as file:
-        writer = csv.writer(file)
-        writer.writerow(new_row)
+def opendb(query, params=None):
+    con = sqlite3.connect('activity_hub.db')
+    cur = con.cursor()
+    if params is None:
+        cur.execute(query)
+    else:
+        cur.execute(query, params)
+    res = cur.fetchall()
+    con.close()
+    return res
+
+def editdb(query, values):
+    con = sqlite3.connect('activity_hub.db')
+    cur = con.cursor()
+    cur.execute(query, values)
+    con.commit()
+    con.close()
+
+def addcolumn(command):
+    conn = sqlite3.connect('activity_hub.db')
+    cursor = conn.cursor()
+    cursor.execute(command)
+    conn.commit()
+    conn.close()
+
 
 @app.route("/")
 def home():
@@ -22,28 +35,28 @@ def home():
 
 @app.route("/announcements")
 def announce():
-    homeworks = []
-    events = []
-    reminders = []
-    with open("announcement.csv", "r") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if row[0] == "homework":
-                homeworks.append(row[1])
-            elif row[0] == "event":
-                events.append(row[1])
-            else:
-                 reminders.append(row[1])
-                
+    homeworks = opendb("SELECT announcement_id, announcement FROM announcements WHERE type = ?", ('homework',))
+    events = opendb("SELECT announcement_id, announcement FROM announcements WHERE type = ?", ('event',))
+    reminders = opendb("SELECT announcement_id, announcement FROM announcements WHERE type = ?", ('reminder',))
+               
     return render_template("announce.html", homeworks=homeworks, events=events, reminders=reminders)
 
 @app.route("/attendance")
 def attendance():
-    content = readcsv("attendance.csv")
-    with open("dates.txt", "r") as file:
-        contents = file.readline()
-        dates = contents.split(".")[:-1]
-    return render_template("attendance.html", content=content, dates=dates)
+    data = opendb("SELECT * FROM attendance")
+    
+    con = sqlite3.connect('activity_hub.db')
+    cur = con.cursor()
+    query = "SELECT * FROM attendance LIMIT 1"
+    cur.execute(query)
+    dates = [description[0] for description in cur.description]
+    formatted_dates = []
+    for date in dates[3:]:
+        split_date = date.split("_")
+        formatted_dates.append(split_date[0] + "/" + split_date[1])
+    dates = formatted_dates       
+    con.close()
+    return render_template("attendance.html", data=data, dates=dates)
 
 @app.route("/schedule")
 def schedule():
@@ -91,50 +104,37 @@ def admin_dashboard():
             reminder = request.form.get('reminder', '').strip()
 
             if homework:
-                addrow("announcement.csv", ["homework", homework])
+                editdb("INSERT INTO announcements (type, announcement) VALUES (?, ?)", ("homework", homework))
 
             if event:
-                addrow("announcement.csv", ["event", event])
+                editdb("INSERT INTO announcements (type, announcement) VALUES (?, ?)", ("event", event))
                 
             if reminder:
-                addrow("announcement.csv", ["reminder", reminder])
+                editdb("INSERT INTO announcements (type, announcement) VALUES (?, ?)", ("reminder", reminder))
 
             return redirect('/admin/control?token=' + token)
         elif type == "student":
             name = request.form.get('name', '').strip()
             stu_class = request.form.get('class', '').strip()
-            addrow("attendance.csv", [name, stu_class])
-            with open("students.txt", "a") as file:
-                file.write(name + ".")
+            editdb("INSERT INTO attendance (name, class) VALUES (?, ?)", (name, stu_class))
             return redirect('/admin/control?token=' + token)
         elif type == "attendance":
             ab_day = request.form.get('ab_day', '').strip()
             month = request.form.get('month', '').strip()
-            date = ab_day + "/" + month
-            with open("dates.txt", "a") as file:
-                file.write(date + ".")
+            date = ab_day + "_" + month
+            date = f"'{date}'"
+            addcolumn((f"ALTER TABLE attendance ADD COLUMN {date} TEXT;"))
             absent_students = request.form.getlist('absent_student')
             vr_students = request.form.getlist('vr_student')
-            attendance_list = readcsv("attendance.csv")
-            new_attendance_list = []
-            for attendance in attendance_list:
-                if attendance[0] in absent_students:
-                    new_attendance_list.append(attendance + [0])
-                elif attendance[0] in vr_students:
-                    new_attendance_list.append(attendance + ["VR"])
-                else:
-                    new_attendance_list.append(attendance + [1])
-            with open("attendance.csv", "w") as file:
-                writer = csv.writer(file)
-                for attendance in new_attendance_list:
-                    writer.writerow(attendance)
-
+            editdb(f"UPDATE attendance SET {date} = ?", ('1',))
+            for student in absent_students:
+                editdb(f"UPDATE attendance SET {date} = ? WHERE name = ?", ('0', student))
+            for student in vr_students:
+                editdb(f"UPDATE attendance SET {date} = ? WHERE name = ?", ('VR', student))
             return redirect('/admin/control?token=' + token)
-    with open("students.txt", "r") as file:
-        contents = file.readline()
-        student_name = contents.split(".")[:-1]
-        
-    
+
+    student_name = opendb("SELECT name, class FROM attendance", None)
+   
     return render_template("control.html", student_name=student_name)
 
 if __name__ == '__main__':
